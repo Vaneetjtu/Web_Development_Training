d34ce7cf0e4bc46a0a4024ed2fa1cb3c12b89ff3
Goyal <vanegoyal@deloitte.com>
2022-10-19 12:29:30 +0530

    Day 13

---
+++ Node/Day 13/Demo.js
@@ -0,0 +1,36 @@
+const product=require("./ProductModule");
+console.log("All Products");
+product.allProducts().map((item)=>{
+    const {Product, Cat, Price, Qty }=item;
+    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
+    console.log(str1);
+})
+console.log("--------------------------------------------------------------------------------------");
+console.log("Products by Veg category");
+product.cat().map((item)=>{
+    const {Product, Cat, Price, Qty }=item;
+    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
+    console.log(str1);
+})
+console.log("--------------------------------------------------------------------------------------");
+console.log("Products between Rs.10-Rs.15");
+product.price().map((item)=>{
+    const {Product, Cat, Price, Qty }=item;
+    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
+    console.log(str1);
+})
+console.log("--------------------------------------------------------------------------------------");
+console.log("Out of stock");
+product.outStock().map((item)=>{
+    const {Product, Cat, Price, Qty }=item;
+    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
+    console.log(str1);
+})
+console.log("--------------------------------------------------------------------------------------");
+console.log("In Stock");
+product.inStock().map((item)=>{
+    const {Product, Cat, Price, Qty }=item;
+    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
+    console.log(str1);
+})
+
