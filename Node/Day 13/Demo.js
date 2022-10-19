6a881ef01a6ca426e7f5ad8dc907cf99271a646d
Goyal <vanegoyal@deloitte.com>
2022-10-19 12:42:04 +0530

    Day 13

--- Node/Day 13/Demo.js
+++ Node/Day 13/Demo.js
@@ -1,37 +1,36 @@
 const product=require("./ProductModule");
-console.log("--------------------------------------------------------------------------------------");
-console.log("All Products");
-product.allProducts().map((item)=>{
+const print=(item)=>{
     const {Product, Cat, Price, Qty }=item;
     let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
     console.log(str1);
+}
+console.log("--------------------------------------------------------------------------------------");
+console.log("All Products");
+product.allProducts().map((item)=>{
+    print(item);
 })
 console.log("--------------------------------------------------------------------------------------");
 console.log("Products by Veg category");
 product.cat().map((item)=>{
-    const {Product, Cat, Price, Qty }=item;
-    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
-    console.log(str1);
+    print(item);
+    
 })
 console.log("--------------------------------------------------------------------------------------");
 console.log("Products between Rs.10-Rs.15");
 product.price().map((item)=>{
-    const {Product, Cat, Price, Qty }=item;
-    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
-    console.log(str1);
+    print(item);
+    
 })
 console.log("--------------------------------------------------------------------------------------");
 console.log("Out of stock");
 product.outStock().map((item)=>{
-    const {Product, Cat, Price, Qty }=item;
-    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
-    console.log(str1);
+    print(item);
+    
 })
 console.log("--------------------------------------------------------------------------------------");
 console.log("In Stock");
 product.inStock().map((item)=>{
-    const {Product, Cat, Price, Qty }=item;
-    let str1=`Product: ${Product}, Category: ${Cat}, Price: ${Price}, Qty: ${Qty}`;
-    console.log(str1);
+    print(item);
+    
 })
 
