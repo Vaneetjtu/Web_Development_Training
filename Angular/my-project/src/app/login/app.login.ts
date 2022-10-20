6bcebaa45466254fe58f58f5893be2af28d65e2b
Goyal <vanegoyal@deloitte.com>
2022-10-20 14:41:40 +0530

    Day 14

---
+++ Angular/my-project/src/app/login/app.login.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.login.html',
+  styleUrls: ['./app.login.css'],
+})
+export class login {
+  title = 'my-project';
+}
