cf515e28e0d34cecb1b51483840ac8bb62c81ea9
Goyal <vanegoyal@deloitte.com>
2022-11-03 12:21:51 +0530

    Day 22

--- Day 22/my-app/src/Product.js
+++ Day 22/my-app/src/Product.js
@@ -5,15 +5,6 @@ const Product=()=>{
     const [qty,setQty]=useState(0);
     const [price,setPrice]=useState(0);
     const [total,setTotal]=useState(0);
-    const inputName=(event)=>{
-        setName(event.target.value);
-    }
-    const inputQty=(event)=>{
-        setQty(event.target.value);
-    }
-    const inputPrice=(event)=>{
-        setPrice(event.target.value);
-    }
     const getTotal=()=>{
         setTotal(price*qty);
     }
@@ -21,9 +12,9 @@ const Product=()=>{
         <>
         <h1>Product Details</h1>
         <hr/>
-        Name: <input type="text" onChange={inputName}/> <br/> <br/>
-        Unit Price: <input type="number" onChange={inputPrice}/> <br/> <br/>
-        Quantity: <input type="number" onChange={inputQty}/> <br/> <br/>
+        Name: <input type="text" onChange={(e)=>{setName(e.target.value)}}/> <br/> <br/>
+        Unit Price: <input type="number" onChange={(e)=>{setPrice(e.target.value)}}/> <br/> <br/>
+        Quantity: <input type="number" onChange={(e)=>{setQty(e.target.value)}}/> <br/> <br/>
         <input type="button" value="Get Total" onClick={getTotal}/> <br/> <br/>
         Product Name: {name} <br/> <br/>
         Total Amount: {total} <br/> <br/>
